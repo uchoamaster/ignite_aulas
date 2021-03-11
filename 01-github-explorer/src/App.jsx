4f57f29e0e3db996003ca9d2a0231821d0c602d2
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-03-11 16:07:46 -0300

    primeiro componente react

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,6 +1,7 @@
+import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 export function App() {
     
-    return <h1>Hello ReactJS</h1>
+    return <RepositoryList />
 }
---
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,75 @@
+const repositoryName = 'unform';
+
+
+export function RepositoryList() {
+    return(
+        <section className="repository-list">
+            <h1>Lista de Repositórios</h1>
+            <ul>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+                    <a href="#">
+                        Acessar Repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+                    <a href="#">
+                        Acessar Repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+                    <a href="#">
+                        Acessar Repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+                    <a href="#">
+                        Acessar Repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+                    <a href="#">
+                        Acessar Repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+                    <a href="#">
+                        Acessar Repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+                    <a href="#">
+                        Acessar Repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+                    <a href="#">
+                        Acessar Repositório
+                    </a>
+                </li>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+                    <a href="#">
+                        Acessar Repositório
+                    </a>
+                </li>
+            </ul>
+        </section>
+    );
+}
