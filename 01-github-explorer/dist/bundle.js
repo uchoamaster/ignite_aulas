470724cf862da8e9fcac906c8bf21fee231f8861
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-03-11 10:39:26 -0300

    ajustando e removendo babel cli para funcionar o compilador do babel

---
+++ 01-github-explorer/dist/bundle.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _user$address;
+
+var user = {
+  name: 'Diego'
+};
+console.log((_user$address = user.address) === null || _user$address === void 0 ? void 0 : _user$address.street);
