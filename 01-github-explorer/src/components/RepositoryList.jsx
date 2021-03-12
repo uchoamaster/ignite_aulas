26284978af49d5d8eb757cbb55bc06c5ccc522a4
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-03-12 09:13:48 -0300

    utilizando useEffect

--- 01-github-explorer/src/components/RepositoryList.jsx
+++ 01-github-explorer/src/components/RepositoryList.jsx
@@ -1,6 +1,10 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
+
+//https://api.github.com/orgs/rocketseat/repos
+
 
 const repository = {
     name: 'unform',
@@ -9,30 +13,18 @@ const repository = {
 }
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+//useEffect serve para disparar uma ação quando algo acontecer na minha aplicação
+useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+    .then(response => response.json())
+    .then(data => setRepositories(data))
+}, [repositories]);
+
     return(
         <section className="repository-list">
             <h1>Lista de Repositórios</h1>
             <ul>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
-                <RepositoryItem repository={repository}/>
                 <RepositoryItem repository={repository}/>
                 <RepositoryItem repository={repository}/>
                 <RepositoryItem repository={repository}/>
