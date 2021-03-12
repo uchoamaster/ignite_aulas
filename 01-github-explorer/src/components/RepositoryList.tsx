6a9a68136ed3bcef055e514c28675f62dd7dd366
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-03-12 15:35:22 -0300

    componentes com typescript

--- 01-github-explorer/src/components/RepositoryList.tsx
+++ 01-github-explorer/src/components/RepositoryList.tsx
@@ -3,10 +3,18 @@ import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss';
 
+interface Repository {
+    name: string;
+    description: string;
+    html_url: string;
+}
+
+
+
 //https://api.github.com/orgs/rocketseat/repos
 
 export function RepositoryList() {
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 //useEffect serve para disparar uma ação quando algo acontecer na minha aplicação
 useEffect(() => {
     fetch('https://api.github.com/users/uchoamaster/repos')
