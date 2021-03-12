49ef4a6da060f82bb3d1d673a4924eef47c28af5
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-03-12 15:00:14 -0300

    adicionando typescript no react

--- 01-github-explorer/webpack.config.js
+++ 01-github-explorer/webpack.config.js
@@ -9,14 +9,14 @@ const isDevelopment = process.env.NODE_ENV != 'production'
 module.exports = {
     mode: isDevelopment ? 'development' : 'production',
     devtool: isDevelopment ?  'eval-source-map' : 'source-map',
-    entry: path.resolve(__dirname,'src', 'index.jsx'),
+    entry: path.resolve(__dirname,'src', 'index.tsx'),
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
 
     resolve: {
-        extensions: [ '.js', '.jsx'],
+        extensions: [ '.js', '.jsx','.ts','.tsx'],
     },
     devServer: {
         contentBase: path.resolve(__dirname, 'public'),
@@ -31,7 +31,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.jsx$/,
+                test: /\.(j|t)sx$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
