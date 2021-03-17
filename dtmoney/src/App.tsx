0ce7a7d22dbcb7ce6f87f613fa2782667a0ad7dc
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-03-17 13:12:01 -0300

    instalando bibliotecas

--- dtmoney/src/App.tsx
+++ dtmoney/src/App.tsx
@@ -1,7 +1,16 @@
+import styled from 'styled-components'
+
+const Title = styled.h1 `
+color: #8257e6;
+font-size:64px;
+`
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title>Hello World
+      </Title>
+
     </div>
   );
 }
