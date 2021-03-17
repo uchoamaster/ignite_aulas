bd5c785019a871003a2018ee31037f3c7927b8ff
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-03-17 14:32:05 -0300

    ajustando a font da aplicação

--- dtmoney/src/styles/global.ts
+++ dtmoney/src/styles/global.ts
@@ -15,6 +15,16 @@ export const GlobalStyle = createGlobalStyle`
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
+}
+
+body, input, textarea, button {
+  font-family: 'Poppins', sans-serif;
+  font-weight: 400; 
+}
+
+h1, h2, h3, h4, h5, h6, strong {
+  font-weight: 600;
 }
 
 html {
