0fe7516379f355b1386e950b6781041b85282271
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-07-15 11:34:51 -0300

    aplicando css no botão

--- dtmoney/src/components/Header/styles.ts
+++ dtmoney/src/components/Header/styles.ts
@@ -2,9 +2,23 @@ import styled from 'styled-components';
 
 export const Container = styled.header `
 background: var(--blue);
+width:100%;
+
 `;
 
 export const Content = styled.div `
 max-width: 1120px;
 margin: 0 auto;
+padding: 0 1rem 10rem;
+display: flex;
+align-items:center;
+justify-content: space-between;
+
+button{
+    font-size: 1rem;
+    color: #fff;
+    background: var(--blue-light);
+    border:0;
+    padding: 0 2rem;
+}
 `;
