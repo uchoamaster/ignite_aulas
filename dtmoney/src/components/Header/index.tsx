5fc0ac3f81853e7cece49131c9b85520bf6bab29
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-07-15 11:00:54 -0300

    aplicando components container e content para centralizar e colocar cores no header

--- dtmoney/src/components/Header/index.tsx
+++ dtmoney/src/components/Header/index.tsx
@@ -1,12 +1,15 @@
 import logoImg from '../../assets/logo.svg'
+import { Container, Content } from './styles'
 
 export function Header(){
     return(
-        <header>
+        <Container>
+            <Content>
             <img src={logoImg} alt="Dt Money" />
             <button type="button">
             Nova Transação
             </button>
-        </header>
+            </Content>
+        </Container>
     )
 }
---
+++ dtmoney/src/components/Header/styles.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const Container = styled.header `
+background: var(--blue);
+`;
+
+export const Content = styled.div `
+max-width: 1120px;
+margin: 0 auto;
+`;
