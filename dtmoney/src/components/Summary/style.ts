c92f10552199b5b7d9cfd2ae7f3c9543d413ebaa
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-07-15 17:06:50 -0300

    finalizando aula 02

--- dtmoney/src/components/Summary/style.ts
+++ dtmoney/src/components/Summary/style.ts
@@ -1,4 +1,29 @@
 import styled from "styled-components";
 
 export const Container = styled.div `
+display: grid;
+grid-template-columns: repeat(3,1fr);
+gap:2rem;
+margin-top: -10rem;
+
+div{
+    background: var(--shape);
+    padding: 1.25rem 2rem;
+    border-radius: 0.25rem;
+    color: var(--title);
+}
+
+header{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+
+strong{
+    margin-top:1rem;
+    font-size:2rem;
+    font-weight: 500;
+    line-height:3rem;
+
+}
 `;
