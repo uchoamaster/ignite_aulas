1328b425ab6c1d1772163c5a830d3b8531d033f9
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-07-15 13:26:17 -0300

    finalizando o Header

--- dtmoney/src/components/Header/styles.ts
+++ dtmoney/src/components/Header/styles.ts
@@ -9,7 +9,7 @@ width:100%;
 export const Content = styled.div `
 max-width: 1120px;
 margin: 0 auto;
-padding: 0 1rem 10rem;
+padding: 2rem 1rem 12rem;
 display: flex;
 align-items:center;
 justify-content: space-between;
@@ -20,5 +20,11 @@ button{
     background: var(--blue-light);
     border:0;
     padding: 0 2rem;
+    border-radius: 0.25rem;
+    height: 3rem;
+    transition: filter 0.2s;
+    &:hover{
+        filter:brightness(0.9);
+    }
 }
 `;
