2987448f5bd660f0f1d3d0eabe7c58fd7e03d8c9
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-07-26 16:34:14 -0300

    ajustes

--- dtmoney/src/components/TransactionsTable/index.tsx
+++ dtmoney/src/components/TransactionsTable/index.tsx
@@ -15,25 +15,25 @@ export function TransactionsTable() {
                 <tbody>
                     <tr>
                         <td>Desenvolvimento de website</td>
-                        <td className="deposit">R$12.000</td>
+                        <td className="deposit">R$ 12.000</td>
                         <td>Desenvolvimento</td>
                         <td>20/02/2021</td>
                     </tr>
                     <tr>
                         <td>Aluguel</td>
-                        <td className="withdraw"> - R$1.100</td>
+                        <td className="withdraw"> - R$ 1.100</td>
                         <td>Casa</td>
                         <td>17/02/2021</td>
                     </tr>
                     <tr>
                         <td>Desenvolvimento de website</td>
-                        <td>R$12.000</td>
+                        <td>R$ 12.000</td>
                         <td>Desenvolvimento</td>
                         <td>20/02/2021</td>
                     </tr>
                     <tr>
                         <td>Computador</td>
-                        <td>R$5.400</td>
+                        <td>R$ 5.400</td>
                         <td>Desenvolvimento</td>
                         <td>20/03/2021</td>
                     </tr>
