17e6fefcceaea55ccde00dbe6a5280c71fb44a24
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-08-16 20:16:06 -0300

    aplicando axios

--- dtmoney/src/components/TransactionsTable/index.tsx
+++ dtmoney/src/components/TransactionsTable/index.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./style";
 
+
 export function TransactionsTable() {
+    useEffect(() => {
+      api.get('transactions')
+      .then(response => console.log(response.data))
+    }, []);
+
     return(
     <Container>
         <table>
--- dtmoney/src/index.tsx
+++ dtmoney/src/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes(){
+    this.namespace = 'api';
+    this.get('/transactions', () => {
+      return [
+      {
+        id: 1, 
+        title: 'Transaction 1',
+        amount: 400,
+        type: 'deposit',
+        category: 'Food',
+        createAt: new Date()
+      }
+    ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
