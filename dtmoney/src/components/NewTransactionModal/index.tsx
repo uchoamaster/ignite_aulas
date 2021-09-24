21c11c003617d4c9064570525dc08ba610289481
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-09-23 21:46:14 -0300

    finalizando estilização e criação dos botoes de entrada e saida da app

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 
 interface NewTransactionModalProps {
     isOpen: boolean;
@@ -32,6 +34,23 @@ export function NewTransactionModal({ isOpen,onRequestClose}:NewTransactionModal
         type="number"
         placeholder="Valor"
         />
+
+        <TransactionTypeContainer>
+        <button
+        type="button"
+        >
+        <img src={incomeImg} alt="Entrada" />
+        <span>Entrada</span>
+        </button>
+        <button
+        type="button"
+        >
+        <img src={outcomeImg} alt="Saída" />
+        <span>Saída</span>
+        </button>
+        </TransactionTypeContainer>
+
+
         <input
         placeholder="Categoria"
         />
