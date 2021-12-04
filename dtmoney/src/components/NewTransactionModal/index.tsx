3392e253fe3f6976c3c2433a48a52a77fb759bdb
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-12-04 09:48:16 -0300

    configurando usecontext no modal de transações

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,13 @@
 import Modal from 'react-modal';
+import { FormEvent, useState, useContext } from 'react';
+import { api } from '../../services/api';
+import { TransactionsContext } from '../../TransactionsContext';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
-import { FormEvent, useState } from 'react';
-import { api } from '../../services/api';
+
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
+
 
 
 interface NewTransactionModalProps {
@@ -13,6 +16,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen,onRequestClose}:NewTransactionModalProps){
+    const transactions = useContext(TransactionsContext);
+
     const [title, setTitle] = useState('');
     const [value, setValue] = useState(0);
     const [category, setCategory] = useState('');
