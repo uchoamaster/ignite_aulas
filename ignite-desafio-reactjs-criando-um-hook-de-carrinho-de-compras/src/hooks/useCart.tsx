d585155205c615e900e2f84597bf4d7bdfbadaab
Carlos Uchoa <carlosjuchoa@hotmail.com>
2021-12-05 14:04:26 -0300

    finalizando carrinho de compra

--- ignite-desafio-reactjs-criando-um-hook-de-carrinho-de-compras/src/hooks/useCart.tsx
+++ ignite-desafio-reactjs-criando-um-hook-de-carrinho-de-compras/src/hooks/useCart.tsx
@@ -83,7 +83,25 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-     
+     if(amount <= 0){
+       return;
+     }
+     const stock = await api.get(`/stock/${productId}`);
+     const stockAmount = stock.data.amount;
+     if(amount > stockAmount){
+       toast.error('Quantidade solicitada fora de estoque');
+       return;
+     }
+
+     const updatedCart = [...cart];
+     const productExists = updatedCart.find(product => product.id === productId);
+      if(productExists){
+        productExists.amount = amount;
+        setCart(updatedCart);
+        localStorage.setItem('@RocketShoes:cart', JSON.stringify(updatedCart))
+      }else{
+        throw Error();
+      }
     } catch {
       toast.error('Erro na alteração de quantidade do produto');
     }
