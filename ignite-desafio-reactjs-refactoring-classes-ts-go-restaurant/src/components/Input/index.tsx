a830884c947786343c556b80abd1eadece858873
Carlos Uchoa <carlosjuchoa@hotmail.com>
2022-07-31 11:20:04 -0300

    change: alterando projeto go restaurant

---
+++ ignite-desafio-reactjs-refactoring-classes-ts-go-restaurant/src/components/Input/index.tsx
@@ -0,0 +1,60 @@
+import {
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from 'react';
+
+import { useField } from '@unform/core';
+import { IconBaseProps } from 'react-icons';
+
+import { Container } from './styles';
+
+interface InputProps {
+  name: string;
+  placeholder?:string;
+  icon?: React.ComponentType<IconBaseProps>;
+}
+
+const Input = ({ name, icon: Icon, ...rest }:InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+
+  const { fieldName, defaultValue, registerField } = useField(name);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+
+    setIsFilled(!!inputRef.current?.value);
+  }, []);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
+  return (
+    <Container isFilled={isFilled} isFocused={isFocused}>
+      {Icon && <Icon size={20} />}
+
+      <input
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
+        defaultValue={defaultValue}
+        ref={inputRef}
+        {...rest}
+      />
+    </Container>
+  );
+};
+
+export default Input;
